1b5ee75d6974db46f9d2752837e1a1e6d5acd5b6
rsimari <rsimari@nd.edu>
2018-09-29 15:25:18 -0700

    first tests

--- test/test_crypto_utils.py
+++ test/test_crypto_utils.py
@@ -7,3 +7,11 @@ from crypto_utils import *
 
 def test_gen_key_pair():
 	pass
+
+def test_write_key():
+	key = bytearray(2048)
+	# write_key(key, '.test_key')
+
+def test_hash_data():
+	data = bytearray(2048)
+	assert hash_data(data) != None
